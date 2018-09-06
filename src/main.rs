f89fb5ec7543aec0f42c5142d6e21fdf4f6cc005
Michael Fairley <michaelfairley@gmail.com>
2018-09-05 22:37:12 -0500

    Trait for comics and general implementation of the rest

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,140 @@ extern crate serde_json;
 use std::{fs,io};
 use std::collections::HashSet;
 
-#[derive(Copy,Clone)]
-enum Comic {
-  PoorlyDrawnLines,
-  WebcomicName,
-}
-
-impl Comic {
-  fn username(self) -> &'static str {
-    use Comic::*;
-    match self {
-      PoorlyDrawnLines => "poorlydrawnlines",
-      WebcomicName => "webcomicname",
-    }
-  }
-
-  fn icon_url(self) -> &'static str {
-    use Comic::*;
-    match self {
-      PoorlyDrawnLines => "https://pbs.twimg.com/profile_images/785967542380617728/Iy0lhx2T.jpg",
-      WebcomicName => "http://68.media.tumblr.com/avatar_44d7cb4c7049_128.png",
-    }
-  }
-}
-
 fn main() {
-  pdl();
-  webcomicname();
+  go::<PDL>();
+  go::<WCN>();
+}
+
+fn go<C: Comic>() {
+  let existing = existing::<C>();
+  let current = C::get_current();
+
+  let new = current.iter().filter(|i| !existing.contains(&i.guid)).collect::<Vec<_>>();
+
+  let f = fs::OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open(C::EXISTING_FILE_NAME)
+    .unwrap();
+
+  for instance in new {
+    use std::io::Write;
+
+    let message = if let Some(title) = &instance.title {
+      format!("{}\n{}", title, instance.image_url)
+    } else {
+      instance.image_url.clone()
+    };
+
+    post_to_slack::<C>(&message);
+
+    writeln!(&f, "{}", instance.guid).unwrap();
+    println!("{}", instance.guid);
+  }
+}
+
+fn existing<C: Comic>() -> HashSet<String> {
+  let mut set = HashSet::new();
+
+  if let Ok(f) = fs::File::open(C::EXISTING_FILE_NAME) {
+    use std::io::BufRead;
+
+    let f = io::BufReader::new(f);
+    set.extend(f.lines().map(Result::unwrap));
+  }
+
+  set
 }
 
 
-fn post_to_slack(comic: Comic, message: String) {
+
+struct Instance {
+  guid: String,
+  title: Option<String>,
+  image_url: String,
+}
+
+trait Comic {
+  const USERNAME: &'static str;
+  const ICON_URL: &'static str;
+  const EXISTING_FILE_NAME: &'static str;
+
+  fn get_current() -> Vec<Instance>;
+}
+
+struct PDL;
+impl Comic for PDL {
+  const USERNAME: &'static str = "poorlydrawnlines";
+  const ICON_URL: &'static str = "https://pbs.twimg.com/profile_images/785967542380617728/Iy0lhx2T.jpg";
+  const EXISTING_FILE_NAME: &'static str = "pdl";
+
+  fn get_current() -> Vec<Instance> {
+    let response = reqwest::get("http://feeds.feedburner.com/PoorlyDrawnLines?format=xml").expect("Bad response");
+
+    let buf_response = io::BufReader::new(response);
+    let channel = rss::Channel::read_from(buf_response).expect("Couldn't read channel");
+
+    channel.items.into_iter().filter_map(|item| {
+      let title = item.title.unwrap().to_lowercase();
+      let guid = item.guid.unwrap().value;
+      let body = item.content.expect("No content").parse::<xml::Element>().expect("Couldn't parse content");
+
+      if let Some(image) = body.get_child("img", None) {
+        let image_url = image.get_attribute("src", None).expect("No src");
+        Some(Instance{
+          guid,
+          title: Some(title),
+          image_url: image_url.to_string(),
+        })
+      } else {
+        None
+      }
+    }).collect()
+  }
+}
+
+struct WCN;
+impl Comic for WCN {
+  const USERNAME: &'static str = "webcomicname";
+  const ICON_URL: &'static str = "http://68.media.tumblr.com/avatar_44d7cb4c7049_128.png";
+  const EXISTING_FILE_NAME: &'static str = "wcn";
+
+
+  fn get_current() -> Vec<Instance> {
+    let response = reqwest::get("http://webcomicname.com/rss").expect("Bad response");
+
+    let buf_response = io::BufReader::new(response);
+    let channel = rss::Channel::read_from(buf_response).expect("Couldn't read channel");
+
+    channel.items.into_iter().filter_map(|item| {
+      let guid = item.guid.unwrap().value;
+      let image = item.description.expect("No description").parse::<xml::Element>().expect("Couldn't parse description");
+
+      let img = if image.name == "img" {
+        Some(&image)
+      } else if let Some(img) = image.get_child("img", None) {
+        Some(img)
+      } else { None };
+
+      img.map(|img| {
+        let image_url = img.get_attribute("src", None).expect("No src");
+        let image_url = image_url.replace("_500.png", "_1280.png");
+        Instance{
+          guid,
+          title: None,
+          image_url: image_url.to_string(),
+        }
+      })
+    }).collect()
+  }
+}
+
+
+fn post_to_slack<C: Comic>(message: &str) {
   let json = json!({
-    "username": comic.username(),
-    "icon_url": comic.icon_url(),
+    "username": C::USERNAME,
+    "icon_url": C::ICON_URL,
     "text": message,
   });
 
@@ -50,112 +150,4 @@ fn post_to_slack(comic: Comic, message: String) {
     .json(&json)
     .send()
     .expect("Posting to Slack failed");
-}
-
-fn webcomicname() {
-  let existing = existing_wcns();
-  let current = get_wcns();
-
-  let new = current.iter().filter(|&&(ref guid, _)| !existing.contains(guid)).collect::<Vec<_>>();
-
-  let f = fs::OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open("wcn").unwrap();
-
-  for &(ref guid, ref url) in new {
-    use std::io::Write;
-
-    post_to_slack(Comic::WebcomicName, format!("{}", url));
-
-    writeln!(&f, "{}", guid).unwrap();
-    println!("{}", guid);
-  }
-}
-
-fn existing_wcns() -> HashSet<String> {
-  let mut set = HashSet::new();
-
-  if let Ok(f) = fs::File::open("wcn") {
-    use std::io::BufRead;
-
-    let f = io::BufReader::new(f);
-    set.extend(f.lines().map(Result::unwrap));
-  }
-
-  set
-}
-
-fn get_wcns() -> Vec<(String, String)> {
-  let response = reqwest::get("http://webcomicname.com/rss").expect("Bad response");
-
-  let buf_response = io::BufReader::new(response);
-  let channel = rss::Channel::read_from(buf_response).expect("Couldn't read channel");
-
-  channel.items.into_iter().filter_map(|item| {
-    let guid = item.guid.unwrap().value;
-    let image = item.description.expect("No description").parse::<xml::Element>().expect("Couldn't parse description");
-
-    if image.name == "img" {
-      let image_url = image.get_attribute("src", None).expect("No src");
-      let image_url = image_url.replace("_500.png", "_1280.png");
-      Some((guid, image_url.to_string()))
-    } else {
-      None
-    }
-  }).collect()
-}
-
-fn pdl() {
-  let existing = existing_pdls();
-  let pdls = get_pdls();
-
-  let new = pdls.iter().filter(|&&(ref guid, _, _)| !existing.contains(guid)).collect::<Vec<_>>();
-
-  let f = fs::OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open("pdl").unwrap();
-
-  for &(ref guid, ref title, ref url) in new {
-    use std::io::Write;
-
-    post_to_slack(Comic::PoorlyDrawnLines, format!("{}\n{}", title, url));
-
-    writeln!(&f, "{}", guid).unwrap();
-    println!("{}", guid);
-  }
-}
-
-fn existing_pdls() -> HashSet<String> {
-  let mut set = HashSet::new();
-
-  if let Ok(f) = fs::File::open("pdl") {
-    use std::io::BufRead;
-
-    let f = io::BufReader::new(f);
-    set.extend(f.lines().map(Result::unwrap));
-  }
-
-  set
-}
-
-fn get_pdls() -> Vec<(String, String, String)> {
-  let response = reqwest::get("http://feeds.feedburner.com/PoorlyDrawnLines?format=xml").expect("Bad response");
-
-  let buf_response = io::BufReader::new(response);
-  let channel = rss::Channel::read_from(buf_response).expect("Couldn't read channel");
-
-  channel.items.into_iter().filter_map(|item| {
-    let title = item.title.unwrap().to_lowercase();
-    let guid = item.guid.unwrap().value;
-    let body = item.content.expect("No content").parse::<xml::Element>().expect("Couldn't parse content");
-
-    if let Some(image) = body.get_child("img", None) {
-      let image_url = image.get_attribute("src", None).expect("No src");
-      Some((guid, title, image_url.to_string()))
-    } else {
-      None
-    }
-  }).collect()
 }
